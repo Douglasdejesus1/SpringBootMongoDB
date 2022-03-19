11d8d420c7674e3915d81672c334a70d85f3b8ab
Douglasdejesus1 <douglas.dj1242@gmail.com>
2022-03-19 08:18:05 -0300

    Consulta com varios critérios Query

--- src/main/java/com/douglas/workshopmongo/services/PostService.java
+++ src/main/java/com/douglas/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.douglas.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,13 +21,18 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		}
+	}
+	
 	public Post insert(Post obj) {
 		return repo.insert(obj);
 	}
 	
 	public List<Post> FindByTitle(String text){
 		return repo.searchTitle(text);
+	}
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 	
 }
